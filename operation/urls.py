12a7afe162a27ac1904eacd000a8a1a1a1aa2ba4
Jose <jeviscarra@gmail.com>
2022-06-26 21:58:58 -0400

    Ajustment
    Recreated navbar, and order operation fields correctly

--- operation/urls.py
+++ operation/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.operation_main, name='home' ),
+    path('',views.dashboard, name='dashboard' ),
+    path('production/', views.production, name='production'),
+    path('orders/', views.orders, name='orders'),
     path('customer/', views.customer, name='customer' ),
 ]
--- operation/views.py
+++ operation/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def operation_main(request):
-    return render(request, 'operation/operationmain.html' )
+def dashboard(request):
+    return render(request, 'operation/dashboard.html')
+
+def production(request):
+    return render(request, 'operation/production.html')
+
+def orders(request):
+    return render(request, 'operation/orders.html')
 
 def customer(request):
     return render(request, 'operation/customer.html')
